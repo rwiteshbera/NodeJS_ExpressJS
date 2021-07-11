f09a4f5de55e9f0e4eed9a9062a94b08b8099890
rwiteshbera <rwiteshbera@gmail.com>
2021-07-11 22:33:18 +0530

    Initial Commit

---
+++ importExport/index.js
@@ -0,0 +1,11 @@
+// const operator = require("./operator");
+
+// console.log(operator.add(10,5));
+// console.log(operator.sub(10,7));
+
+
+const {add, sub, mult} = require("./operator");
+
+console.log(add(10,5));
+console.log(sub(10,7));
+console.log(mult(10,7));
