6c26e9a12d0b7ab8e0f3303e96ab85688058dd05
rwiteshbera <rwiteshbera@gmail.com>
2021-07-21 20:27:38 +0530

    Updated express.js code

---
+++ Express.js/routing_html.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3500;
+
+
+const staticPath = path.join(__dirname, "./SampleWebpage");
+
+//built-in middleware
+app.use(express.static(staticPath));
+
+app.get('/', function(req, res) {
+   res.send("Hello");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+}); 
