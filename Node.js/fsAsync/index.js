cb93cfce8af523c46b726a234f4daae687c4648a
rwiteshbera <rwiteshbera@gmail.com>
2021-12-05 13:12:08 +0530

    Updated branch

--- Node.js/fsAsync/index.js
+++
@@ -1,9 +0,0 @@
-var fs = require("fs");
-
-// fs.writeFile("biodata.txt","Today is awesome day.", (err) => {
-//     console.log("File is created");
-// });
-
-fs.readFile("./biodata.txt", "UTF-8",(err,data) => {
-    console.log(data);
-})
